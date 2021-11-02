2e5e84a9a76410bcc9a71b9c536d3d1db0484864
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-02 12:47:10 +0200

    Implementing callbacks for problem-solving

--- 01_04/_scripts/ajax.js
+++ 01_04/_scripts/ajax.js
@@ -1,8 +1,14 @@
 // add global variable containing XHR object here
-
+let httpRequest = new XMLHttpRequest()
 
 // add get() function here
-
+function get(url, success) {
+    httpRequest.open('GET', url)
+    httpRequest.onload = function () {
+        success(httpRequest.responseText)
+    }
+    httpRequest.send()
+}
 
 function tempToF(kelvin) {
     return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
@@ -30,8 +36,9 @@ function successHandler(data) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
+    const apiKey = '7846cfbfd1b49dad9cb82c1ff2a4250e'; // ADD YOUR API KEY BETWEEN THE QUOTES
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
     // add get() function call here
-    
+    get(url, successHandler)
+    // successHandler(httpRequest.responseText)
 });
