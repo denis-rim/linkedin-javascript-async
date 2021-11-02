a70a537028c378429ca5d662b818812f18cf4fe5
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-11-02 13:05:03 +0200

    Building asynchronous code with promises

--- 01_04/_scripts/ajax.js
+++ 01_04/_scripts/ajax.js
@@ -1,27 +1,29 @@
 // add global variable containing XHR object here
-let httpRequest = new XMLHttpRequest()
 
 // add get() function here
-function get(url, success, fail) {
-    httpRequest.open('GET', url)
+function get(url) {
+  return new Promise(function (resolve, reject) {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("GET", url);
     httpRequest.onload = function () {
-        if (httpRequest.status === 200) {
-        success(httpRequest.responseText)
-        } else {
-            fail(httpRequest.status)
-        }
-    }
-    httpRequest.send()
+      if (httpRequest.status === 200) {
+        resolve(httpRequest.responseText);
+      } else {
+        reject(Error(httpRequest.status));
+      }
+    };
+    httpRequest.send();
+  });
 }
 
 function tempToF(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+  return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
 function successHandler(data) {
-    const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
+  const dataObj = JSON.parse(data);
+  const weatherDiv = document.querySelector("#weather");
+  const weatherFragment = `
         <h1>Weather</h1>
         <h2 class="top">
         <img
@@ -32,24 +34,29 @@ function successHandler(data) {
         />${dataObj.name}
         </h2>
         <p>
-        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
+        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${
+    dataObj.weather[0].description
+  }
         </p>
-    `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
+    `;
+  weatherDiv.innerHTML = weatherFragment;
+  weatherDiv.classList.remove("hidden");
 }
 
 function failHandler(status) {
-    console.log(status)
-    const weatherDiv = document.querySelector('#weather');
-    weatherDiv.classList.remove('hidden');
+  console.log(status);
+  const weatherDiv = document.querySelector("#weather");
+  weatherDiv.classList.remove("hidden");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    // const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
-    const apiKey = '7846cfbfd1b49dad9cb82c1ff2a4250e'; // ADD YOUR API KEY BETWEEN THE QUOTES
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
-    // add get() function call here
-    get(url, successHandler, failHandler)
-    // successHandler(httpRequest.responseText)
+document.addEventListener("DOMContentLoaded", function () {
+  // const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
+  const apiKey = "7846cfbfd1b49dad9cb82c1ff2a4250e"; // ADD YOUR API KEY BETWEEN THE QUOTES
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=" +
+    apiKey;
+  // add get() function call here
+  // get(url, successHandler, failHandler)
+  // successHandler(httpRequest.responseText)
+  console.log(get(url));
 });
